104383dabe80320aa65d78634245ebbd737e9776
Rémi Remy <meltingpot54@gmail.com>
2021-09-24 15:44:45 +0200

    menu burger fluide

---
+++ js/burger.js
@@ -0,0 +1,15 @@
+//***************************  Version Jquery ***************************
+
+// $(".burger").click(() => {
+//     $(".listeMenu").slideToggle()
+// })
+
+// *************************** Version javascript **********************************
+
+const button = document.querySelector(".burger") 
+const menu = document.querySelector(".listeMenu")
+
+button.addEventListener("click", event => {
+    menu.classList.toggle("show")
+})
+
